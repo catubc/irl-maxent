eb7c1a3aee2c3362850ed5d0d03403d559246326
catubc <mitelutco@gmail.com>
2022-09-07 07:01:50 +0200

    updated

--- src/irl_maxent/utils.py
+++ src/irl_maxent/utils.py
@@ -59,9 +59,12 @@ def generate_expert_trajectories(world, reward, terminal):
 #
 def plot_reward_and_expert_trajectories(world,
                                         reward,
-                                        style,
+                                        #style,
                                         expert_policy,
                                         trajectories):
+    style = {                                   # global style for plots
+        'border': {'color': 'red', 'linewidth': 0.5},
+    }
 
     #
     fig = plt.figure()
@@ -86,3 +89,79 @@ def plot_reward_and_expert_trajectories(world,
     fig.tight_layout()
     plt.show()
 
+def plot_trajectory(traj):
+
+    # compute locations
+    loc_ids = np.arange(25)
+    loc_xy = np.zeros((loc_ids.shape[0],2))
+    for k in range(loc_ids.shape[0]):
+        #print (loc_xy[k], )
+        loc_xy[k] = [loc_ids[k]%5, loc_ids[k]//5]
+        #print (k, loc_xy[k])
+
+    #
+    colors = plt.cm.jet(np.linspace(0,1,traj.shape[0]))
+
+    # make grid world:
+    for k in range(6):
+        plt.plot([0,5],[k,k],
+                 c='black')
+        #break
+    for k in range(6):
+        plt.plot([k,k],[0,5],
+                 c='black')
+
+    for k in range(traj.shape[0]):
+        t = traj[k]
+        s = traj[k][0]
+        e = traj[k][2]
+
+        #
+        #print ("k: ", k, ", s, e: ", s,e, "s xy: ", loc_xy[s], " e xy: ", loc_xy[e])
+        temp = np.vstack((loc_xy[s], loc_xy[e]))
+        plt.plot(temp[:,0]+0.5, temp[:,1]+0.5,
+                  c=colors[k])
+    #
+
+
+##################################################
+# # Let's start with the easy parts:
+# Remember that the gradient we need to compute for optimization is
+#
+# $$
+#     \nabla_\omega \mathcal{L}(\omega)
+#     = \underbrace{\mathbb{E}_{\pi^E} \left[ \phi(\tau) \right]}_{(1)}
+#         - \underbrace{\sum_{s_i} D_{s_i} \phi(s_i)}_{(2)},
+# $$
+#
+# so we first need to get the feature expectation $\mathbb{E}_{\pi^E} \left[ \phi(\tau) \right]$ (part (1)) from the trajectories of our trainings-set $\mathcal{D}$.
+# This is fairly simple, as for each trajectory we just need to check which states are going to be visited, sum the features of these states up (counting repetitions), and then at the end average over all trajectories, i.e.
+#
+# $$
+#     \mathbb{E}_{\pi^E} \left[ \phi(\tau) \right]
+#     = \frac{1}{|\mathcal{D}|} \sum_{\tau \in \mathcal{D}} \sum_{s_t \in \tau} \phi(s_t)
+# $$
+#
+# or as a function:
+def feature_expectation_from_trajectories(features, trajectories):
+    n_states, n_features = features.shape
+
+    fe = np.zeros(n_features)
+
+    for t in trajectories:                  # for each trajectory
+        for s in t.states():                # for each state in trajectory
+            fe += features[s, :]            # sum-up features
+
+    return fe / len(trajectories)           # average over trajectories
+
+#########################################################
+# Another fairly simple thing we need to know about the trajectories is the probability of a state being a starting state, $p(s_0)$.
+# This is required for the state-visitation-frequency $D_{s_i}$ which we in turn need for part (2) of the gradient.
+# Here, we just count the number of trajectories starting in each state and then normalize with the number of trajectories.
+def initial_probabilities_from_trajectories(n_states, trajectories):
+    p = np.zeros(n_states)
+
+    for t in trajectories:                  # for each trajectory
+        p[t.transitions()[0][0]] += 1.0     # increment starting state
+
+    return p / len(trajectories)            # normalize
