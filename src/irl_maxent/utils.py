235d0d973f0d3325b82f2976cd8f23165e3a1cd8
cat <mitelutco@gmail.com>
2022-09-11 19:27:55 +0200

    updates

--- src/irl_maxent/utils.py
+++ src/irl_maxent/utils.py
@@ -6,6 +6,7 @@ thereof.
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
+from tqdm import trange
 
 #
 import gridworld as W                       # basic grid-world MDPs
@@ -89,7 +90,8 @@ def plot_reward_and_expert_trajectories(world,
     fig.tight_layout()
     plt.show()
 
-def plot_trajectory(traj,n_col,n_row):
+#
+def plot_trajectory_simulated(traj,n_col,n_row):
 
     #
     #print ("plotting trajectory: ", traj)
@@ -133,7 +135,300 @@ def plot_trajectory(traj,n_col,n_row):
         temp = np.vstack((loc_xy[s], loc_xy[e]))
         plt.plot(temp[:,0]+0.5, temp[:,1]+0.5,
                   c=colors[k])
+
+
+#
+def plot_trajectory_gerbils(traj,
+                    n_col,
+                    n_row,
+                    grid_states,
+                    plot_text=True):
+
     #
+    colors = plt.cm.jet(np.linspace(0,1,traj.shape[0]))
+
+    # make grid world:
+    for k in range(n_row+2):
+        plt.plot([0,n_col-1],
+                 [k-1,k-1],
+                 c='black',
+                 alpha=0.2)
+        #break
+    for k in range(n_col):
+        plt.plot([k,k],
+                 [-1,n_row],
+                 c='black',
+                 alpha=0.2)
+    #
+    if plot_text:
+        for k in range(n_col*n_row):
+            x = k//n_col+0.5
+            y = n_row-(k)%n_col-0.5
+            #print ("x, y: ", x,y, k//n_row,-(k)//n_col+n_row)
+            plt.text(x,
+                     y,
+                     str(k),
+
+                     fontsize=6)
+
+
+    #print ("plotted traj locations: ", np.vstack(traj))
+    noise_old = np.random.rand(2) / 5. - 0.2
+    for k in range(traj.shape[0]):
+        #t = traj[k]
+        s = traj[k][0]
+        e = traj[k][2]
+
+        #
+        noise_new = np.random.rand(2) / 5. - 0.2
+        # print ("k: ", k, ", s, e: ", s,e, "s xy: ", loc_xy[s], " e xy: ", loc_xy[e])
+
+        #
+        idx_s = np.where(grid_states==s)
+        idx_e = np.where(grid_states==e)
+
+        #print ("Idx s: ", idx_s,
+        #       " idx_e,: ", idx_e)
+        temp = np.vstack((np.hstack(idx_s)[::-1]+noise_old,
+                          np.hstack(idx_e)[::-1]+noise_new))
+        noise_old = noise_new
+
+        #
+        plt.plot(temp[:,0]+0.5,
+                 n_row-temp[:,1]-0.5,
+                  c=colors[k])
+
+    #
+
+
+# @jit
+def fix_trajectories(track_xy1,
+                     max_jump_allowed=100,
+                     max_dist_to_join=100,
+                     min_chunk_len=5,
+                     plotting=False):
+    ''' Method to fix the large jumps, short orphaned segments,
+        and interpolate across short distances
+
+        Input:
+        - track array for a single animal: [n_time_steps, 2], where the 2 is for x-y locations
+        - max_jump_allowed: maximum number of pixels (in euclidean distance) a track is allowed to jump before being split
+        - max_dist_to_join: when joining orphaned tracks, the largest distacne allowed between 2 track bouts
+        - min_chunk_len = shortest chunk allowed (this is applied at the end after joining all the chunks back
+
+        Output: fixed track
+
+    '''
+
+    ########################################
+    ###########  Delete big jumps ##########
+    ########################################
+    for k in trange(1, track_xy1.shape[0] - 1, 1):
+        if np.linalg.norm(track_xy1[k] - track_xy1[k - 1]) > max_jump_allowed:
+            track_xy1[k] = np.nan
+
+    ########################################
+    ##### Join segments that are close #####
+    ########################################
+    #
+    last_chunk_xy = None
+
+    # check if we start outside chunk or inside
+    if np.isnan(track_xy1[0, 0]) == False:
+        inside = True
+    else:
+        inside = False
+
+    # interpolate between small bits
+    for k in trange(1, track_xy1.shape[0] - 1, 1):
+        if np.isnan(track_xy1[k, 0]):
+            if inside:
+                inside = False
+                last_chunk_xy = track_xy1[k]
+                last_chunk_idx = k
+        else:
+            if inside == False:
+                inside = True
+                new_chunk_xy = track_xy1[k]
+                new_chunk_idx = k
+                if last_chunk_xy is not None:
+                    dist = np.linalg.norm(track_xy1[k] - track_xy1[k - 1])
+                    if dist <= max_dist_to_join:
+                        track_xy1[last_chunk_idx:new_chunk_idx] = new_chunk_xy
+
+    ########################################
+    ##  Delete short segments left behind ##
+    ########################################
+    #
+
+    chunk_start_xy = None
+
+    # check if we start outside chunk or inside
+    if np.isnan(track_xy1[0, 0]) == False:
+        chunk_start_idx = 0
+        inside = True
+    else:
+        inside = False
+
+    # interpolate between small bits
+    for k in trange(1, track_xy1.shape[0] - 1, 1):
+        if np.isnan(track_xy1[k, 0]):
+            if inside:
+                inside = False
+                chunk_end_idx = k
+                if (chunk_end_idx - chunk_start_idx) < min_chunk_len:
+                    track_xy1[chunk_start_idx:chunk_end_idx] = np.nan
+        else:
+            if inside == False:
+                inside = True
+                chunk_start_idx = k
+
+    if plotting:
+        fig = plt.figure()
+        plt.plot(track_xy1[:, 0],
+                 track_xy1[:, 1])
+        plt.show()
+
+    return track_xy1
+
+
+
+#
+def compute_trajectories_from_data(track,
+                                   box_width,
+                                   box_height,
+                                   remove_no_movement_states_flag=True,
+                                   max_len_trajectory=20,
+                                   grid_width=100,
+                                   min_len_trajectory=10):
+
+    #
+    verbose = False
+
+    #
+    n_col = box_width // grid_width
+    n_row = box_height // grid_width
+
+    # make a 2d array holding all the state locations
+    n_states = n_row * n_col
+    print(" n states: ", n_states, ' n_col ', n_col, " n_row ", n_row)
+    grid_states = np.arange(n_states, dtype='int32')
+    grid_states = np.array_split(grid_states, n_row)
+    grid_states = np.vstack(grid_states).T  # [::-1].T
+    print("grid states: ", grid_states)
+
+    # convert all trajectories to discrete grid world xy values
+    track_xy = track.copy()
+    track_xy1 = track_xy[:, 1]
+
+    ################################################
+    ############ FIX TRACK ERRRORS #################
+    ################################################
+    max_jump_allowed = np.sqrt(2 * grid_width ** 2)
+    max_dist_to_join = np.sqrt(2 * grid_width ** 2)
+    # min_chunk_len=5,
+
+    track_xy1 = fix_trajectories(track_xy1,
+                                 max_jump_allowed,
+                                 max_dist_to_join)
+
+    #
+    track_xy1 = np.float32(track_xy1 // grid_width)
+
+    #
+    track_states1 = np.int32(track_xy1[:, 0] + track_xy1[:, 1] * n_col)
+
+    # list to save states of each animal
+    states1 = []
+    actions_xy1 = []
+
+    # get first state for first animal
+    s = track_states1[0]
+    e = track_states1[1]
+    s_xy = track_xy1[0]  # start xy
+    e_xy = track_xy1[1]  # end xy
+
+    #
+    active_trajectory = []
+    active_trajectory = update_trajectory(active_trajectory,
+                                          s_xy,
+                                          e_xy,
+                                          s,
+                                          e,
+                                          grid_states)
+    # delete the state
+    track_states1 = np.delete(track_states1, 0, axis=0)
+    track_xy1 = np.delete(track_xy1, 0, axis=0)
+
+    # loop over remaining states
+    ctr = 0
+    while track_states1.shape[0] > 1:
+
+        # get first animal locations
+        s = track_states1[0]
+        e = track_states1[1]
+        s_xy = track_xy1[0]  # start xy
+        e_xy = track_xy1[1]  # end xy
+
+        # do a quick check to make sure start and end are nearby:
+        delta_x = np.abs(s_xy[0] - e_xy[0])
+        delta_y = np.abs(s_xy[1] - e_xy[1])
+        if (delta_x + delta_y) <= 2 and np.any(np.isnan(s_xy)) == False and np.any(np.isnan(e_xy)) == False:
+
+            # if len(states1)==2:
+            # print ("delta x, y:", delta_x, delta_y,
+            #       "sxy: ", s_xy, e_xy,
+            #       's, e: ', s,e,
+            #       ", active_trajectory: ", active_trajectory)
+            active_trajectory = update_trajectory(active_trajectory,
+                                                  s_xy,
+                                                  e_xy,
+                                                  s,
+                                                  e,
+                                                  grid_states)
+
+            # TODO: note this conditional essentially removes all states where the gerbils do not do anything;
+            # if remove_no_movement_states_flag:
+            #    if a!=4:
+            #        #print ("up-to-date active: ", active_trajectory)
+            #        active_trajectory.append([s,a,e])
+            # else:
+            #    active_trajectory.append([s,a,e])
+
+            if len(active_trajectory) == max_len_trajectory:
+                states1.append(active_trajectory)
+                if verbose:
+                    print("completed trajectory :", len(states1) - 1)
+                    print("trajectory: ", active_trajectory)
+                    print("##########################################################")
+                    print("")
+                    print("")
+                # print (ctr, "appended trajectroy: ", len(active_trajectory),
+                #       active_trajectory)
+                active_trajectory = []
+        else:
+            if len(active_trajectory) >= min_len_trajectory:
+                states1.append(active_trajectory)
+                if verbose:
+                    print("completed trajectory :", len(states1) - 1)
+                    print("trajectory: ", active_trajectory)
+                    print("############ TRAJECTORY " + str(len(states1) - 1) + "######################")
+                    print("")
+                    print("")
+                # print (ctr, "appended trajectroy: ", len(active_trajectory),
+                #       active_trajectory)
+            active_trajectory = []
+
+        #
+        # delete current value
+        track_states1 = np.delete(track_states1, 0, axis=0)
+        track_xy1 = np.delete(track_xy1, 0, axis=0)
+
+        #
+        ctr += 1
+
+    return states1, actions_xy1, n_col, n_row, grid_states
+
 
 
 ##################################################
@@ -177,3 +472,140 @@ def initial_probabilities_from_trajectories(n_states, trajectories):
         p[t.transitions()[0][0]] += 1.0     # increment starting state
 
     return p / len(trajectories)            # normalize
+
+
+#
+
+
+#
+def get_action_xy_shifts(a):
+    # compute actions
+    # actions = [0, 1, 2, 3, 4]
+    # action_names = ['right', 'left', 'up', 'down', 'nothing']
+
+    if a == 0:
+        shifts = [+1, 0]
+    elif a == 1:
+        shifts = [-1, 0]
+    elif a == 2:
+        shifts = [0, +1]
+    elif a == 3:
+        shifts = [0, -1]
+    elif a == 0:
+        shifts = [0, 0]
+    else:
+        print("ERROR computeing direction:")
+        errror
+
+    return np.int32(shifts)
+
+#
+def update_trajectory(active_trajectory,
+                      start_xy,
+                      end_xy,
+                      start_state,
+                      end_state,
+                      grid_states):
+
+    # compute actions
+    actions = [0, 1, 2, 3, 4]
+    action_names = ['right', 'left', 'up', 'down', 'nothing']
+
+    # compute actions
+    if (start_xy[0] - end_xy[0]) == 0 and (start_xy[1] - end_xy[1]) == 0:
+        action = [4]
+    elif ((start_xy[0] - end_xy[0]) == -1) and ((start_xy[1] - end_xy[1]) == 0):
+        action = [1]
+    elif ((start_xy[0] - end_xy[0]) == +1) and ((start_xy[1] - end_xy[1]) == 0):
+        action = [0]
+    elif ((start_xy[1] - end_xy[1]) == -1) and ((start_xy[0] - end_xy[0]) == 0):
+        action = [3]
+    elif ((start_xy[1] - end_xy[1]) == +1) and ((start_xy[0] - end_xy[0]) == 0):
+        action = [2]
+    else:
+        # DIAGONAL movement
+        if (start_xy[0] - end_xy[0]) == -1:  # right
+            if (start_xy[1] - end_xy[1]) == -1:
+                action = [0, 2]  # up
+            else:
+                action = [0, 3]
+        else:  # left
+            if (start_xy[1] - end_xy[1]) == -1:
+                action = [1, 2]  # up
+            else:
+                action = [1, 3]
+
+        # shuffle the order of the actions because we don't really know which way the animal went
+        # e.g. down, right vs. right, down
+        action = np.random.choice(action, 2, replace=False)
+
+   # print ("action detected: ", action)
+
+
+    # update trajectory
+    if len(action) == 1:
+        if action[0] != 4:
+            transition = [start_state,
+                          action[0],
+                          end_state]
+            active_trajectory.append(transition)
+            #print ("detected regular movement", transition)
+
+    #
+    elif len(action) == 2:
+
+        # select starting state and action for the first action
+        start_state0 = start_state  # this is the starting state
+
+        # here we find which xy directio the action takes us
+        # return a tuple -1,0,1 in each dimension
+        shift = get_action_xy_shifts(action[0])
+        new_x = int(start_xy[0]) + shift[0]
+        new_y = int(start_xy[1]) + shift[1]
+
+        #
+        if False:
+            print ("Diagonal, first step: ", shift,
+               " start_xy: ", start_xy,
+               " end_xy: ", end_xy,
+               " start_state0: ", start_state0,
+               " end_states: ", end_state,
+               " new_x: ", new_x,
+               " new_y: ", new_y
+                   , "grid states shaep: ", grid_states.shape
+               )
+        # here we compute the end state by moving across the grid_states 2d array
+        end_state0 = grid_states[new_x,
+                                 new_y,
+                                 ]
+        # print ("new end state: ", end_state0)
+        # print ("Grid states: ", grid_states, )
+
+        #
+        transition = [start_state0,
+                      action[0],
+                      end_state0]
+        # print ("Appending transition #1: ", transition)
+        #print (" second step")
+
+        #
+        #print ("detected diagonal movment, transiition 1 ", transition)
+        active_trajectory.append(transition)
+
+        # select starting state and action for the second action
+        start_state1 = end_state0
+        end_state1 = end_state  # this is the end state that we expect to end up in
+
+        #
+        transition = [start_state1,
+                      action[1],
+                      end_state1]
+        # print("Appending transition #2: ", transition)
+        # print ("detected diagonal movment, transiition 2 ", transition)
+        active_trajectory.append(transition)
+
+    else:
+        print("ERRROr in action computation: ")
+        error
+
+    return active_trajectory
